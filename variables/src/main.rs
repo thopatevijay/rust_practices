cddd584ccb400bac4bb205adced61c7e20ea1d1d
thopatevijay <thopatevijay@gmail.com>
2023-03-12 23:38:59 +0530

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    
+    constant();
+    shadowing();
+}
+
+fn constant() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    
+    println!("{THREE_HOURS_IN_SECONDS}");
+}
+
+fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
