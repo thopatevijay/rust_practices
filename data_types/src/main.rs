dadcad78e49bb6c941b971b53fc7feb9a2373ca7
thopatevijay <thopatevijay@gmail.com>
2023-03-13 00:15:50 +0530

    data_types

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     numeric_operations();
     boolean_type();
     char_type();
+    tuple_type();
+    array_type();
 }
 
 fn floating_point_types() {
@@ -54,4 +56,34 @@ fn char_type() {
     let heart_eyed_cat = '😻';
 
     println!("c is : {c}, z is : {z}, heart_eyed_cat is : {heart_eyed_cat}");
+}
+
+fn tuple_type() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // destructure
+    let (x, y, z) = tup;
+
+    println!("destructuring values are : {x}, {y}, {z}");
+
+    let first_element = tup.0;
+    // accessing tuple element by period
+    println!("accessing tuple element by period: {first_element}");
+}
+
+fn array_type() {
+    //examples:
+    let a = [1, 2, 3, 4, 5];
+
+    // let months = ["January", "February", "March", "April", "May", "June", "July",
+    //           "August", "September", "October", "November", "December"];
+
+    // let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // let a = [3; 5];
+
+    // Accessing Array Elements
+    let first = a[0];
+
+    println!(" First element is : {first}");
 }
